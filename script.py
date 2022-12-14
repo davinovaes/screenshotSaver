a09e451046f0d5ed472607c95bd996c137504e5c
davinovaes <davinovaes.cel@gmail.com>
2022-12-14 13:51:04 -0300

    status file

--- script.py
+++ script.py
@@ -1,23 +1,31 @@
+from datetime import datetime
 import cv2
 
 file = open("cameralist.csv", "r")
-
-
+file2 = open(f"statusCamera{datetime.now()}.csv", "w")
+i=1
 for line in file:
-    name , rtsp = line.rstrip().split(",")
-    print(name)
-    print(rtsp)
+    name , rtsp = line.rstrip().rsplit(',', 1)
+    print(f"{i} - {name}:", end="")
+    #print(rtsp)
     cap = cv2.VideoCapture(rtsp)
 
     if cap.isOpened():
         ret, frame = cap.read()
         if ret:
-            cv2.imwrite(f'screenshots/{name}.jpg',frame)
-
-        
+            #cv2.imwrite(f'screenshots/{name}.jpg',frame)
+            file2.writelines(f"{name},{rtsp},Online\n")
+            print("Online\n")
+        else:
+            file2.writelines(f"{name},{rtsp},Offline\n")
+            print("Offline\n")       
     else:
-        print("Cannot open camera")
+
+        #print("Cannot open camera")
+        file2.writelines(f"{name},{rtsp},Offline\n")
+        print("Offline\n")
     cap.release()
+    i+=1
 
 
 
